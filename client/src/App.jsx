811900b41f4d162b2a04e87387e8fec5d24cda05
Nikolay-S-Nikolov <nikolay.s.nikolov@gmail.com>
2025-11-06 17:36:01 +0200

    Initial set up

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
